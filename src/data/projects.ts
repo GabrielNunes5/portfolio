12491c591bad34f5330df28cd1c55f48c02dd8ef
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-10-08 21:03:21 -0300

    refactor: :art:

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,7 +18,7 @@ export const projects: Project[] = [
     title: "Landing Page para PetShop",
     description: "Landing page responsiva para um petshop com informações sobre serviços, produtos e contato. O projeto se trata de um desafio de codigo do curso de progamação FullStack da Infinity School.",
     image: "https://i.imgur.com/rEkGXky.png",
-    technologies: ["React", "Tailwind CSS"],
+    technologies: ["React", "Tailwind CSS", "TypeScript", "Vite", "Framer Motion"],
     link: "https://projeto-melhor-amigo-infinity.vercel.app",
     github: "https://github.com/GabrielNunes5/PetShop-Melhor-Amigo",
     challenges: "Criar uma interface responsiva e otimizada seguindo um briefing especificado pelo desafio do curso. O projeto deveria ser simples, mas com um design atraente. Por ser a primeira vez utilizando Tailwind CSS, o desafio foi aprender a utilizar a biblioteca de forma eficiente.",
@@ -27,6 +27,20 @@ export const projects: Project[] = [
       "Design responsivo",
       "Componentes reutilizáveis",
     ],
-
+  },
+  {
+    id: 1,
+    title: "Site para ONG de resgate e adoção de animais",
+    description: "Landing page responsiva para um petshop com informações sobre serviços, produtos e contato. O projeto se trata de um desafio de codigo do curso de progamação FullStack da Infinity School.",
+    image: "https://i.imgur.com/rEkGXky.png",
+    technologies: ["Angular", "Tailwind CSS", "TypeScript"],
+    link: "https://gabrielnunes5.github.io/Amigo-de-Patas-Front",
+    github: "https://github.com/GabrielNunes5/Amigo-de-Patas-Front",
+    challenges: "Criar uma interface responsiva e otimizada seguindo um briefing especificado pelo desafio do curso. O projeto deveria ser simples, mas com um design atraente. Por ser a primeira vez utilizando Tailwind CSS, o desafio foi aprender a utilizar a biblioteca de forma eficiente.",
+    solution: "Utilizei React para criar componentes reutilizáveis e Tailwind CSS para estilização rápida e responsiva, garantindo uma boa experiência em dispositivos móveis. Utilizei bastante da documentação do Tailwind para aprender a biblioteca e suas classes utilitárias.",
+    features: [
+      "Design responsivo",
+      "Componentes reutilizáveis",
+    ],
   }
 ];
